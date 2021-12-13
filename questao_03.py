1524775be1e045e7aa3f146c1b3d0dfe1ea09d8b
Dududenis <dudusjp3@hotmail.com>
2021-12-13 16:51:13 -0300

    Adiciona questoes

---
+++ questao_03.py
@@ -0,0 +1,33 @@
+#3)Faça um programa que percorra duas listas e gere uma terceira sem elementos repetidos.
+primeira = []
+segunda = []
+while True:
+    e = int(input("Digite um valor para a primeira lista (0 para terminar):"))
+    if e == 0:
+        break
+    primeira.append(e)
+while True:
+    e = int(input("Digite um valor para a segunda lista (0 para terminar):"))
+    if e == 0:
+        break
+    segunda.append(e)
+terceira = []
+duas_listas = primeira[:]
+duas_listas.extend(segunda)
+x = 0
+while x < len(duas_listas):
+    y = 0
+    while y < len(terceira):
+        if duas_listas[x] == terceira[y]:
+            break
+        y = y + 1
+    if y == len(terceira):
+        terceira.append(duas_listas[x])
+    x = x + 1
+x = 0
+while x < len(terceira):
+    print(f"{x}: {terceira[x]}")
+    x = x + 1
+        
+    
+    
